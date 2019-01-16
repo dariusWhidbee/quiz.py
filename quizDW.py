7f420a7bee14be95fa09002bdd535ccbb69cbc78
csPython <cspython@AD25TF04NGF1J-78.local>
2019-01-16 13:04:55 -0500

    commented out the score, custom function is defined

--- quizDW.py
+++ quizDW.py
@@ -1,74 +1,49 @@
-# Fix Errors
-
 # Variables
 score = 0
-q1r = False 
-q1a = 1
 
-score = 0
-q2r = False 
-q2a = 2
-
-# Introduction, asks the user 1 question
-print(" Hello, this program runs a quiz with 10 questions."
-      " based on the options under each question. Choose your"
-      " best response")
-print()
-print("""I start off dry but come out wet. I go in light and come out heavy.
-What am I?
-
-1) A teabag
-2) A penis
-3) A cookie 
-4) A spoon""")
-
-# While loop
-while q1r == False:
-    try:
-        print()
-        q1a = int(input("Choose the wisely. > "))
-         # scoreect answer "3"
-        if q1a == 1:
-            score += 1
+def run_quest(quest, check, ansU, ansR):
+    print(quest)
+    while check == False:
+        try:
+            print()
+            ansU = int(input("Responce. > "))
+            # coreect answer "3"
+            if ansU == 1:
+                #global grade
+                #grade += 1
             # if the user answers the question right the boolean turns true
             # and gets out of the while loop
-            q1r = True
-        elif 0 < q1a < 5:
-            q1r = True
+                check = True
+            elif 0 < ansU < 5:
+                check = True
             # If the user doesn't respond with a interger between 1 to 4
-        else:
-            print("please choose an interger from 1 to 4")
+            else:
+                print("please choose an interger from 1 to 4")
             # If the user doesn't respond with a interger
-    except ValueError:
-         print("Invalid response.Please choose either option 1, 2, 3 or 4")
+        except ValueError:
+            print("Invalid response.Please choose either option 1, 2, 3 or 4")
 
 
+# Introduction
+print(" Hello there, new user. This program will run a quiz with 10 questions."
+"Using  your prior knowledge, choose your best response. Good Luck!")
 print()
-print("""When I point up, it’s bright. When I point down, it’s dark.
-What am I?
-1) A flashlight
-2) A light switch
-3) A lamp
-4) The sun and moon""")
 
-while q2r == False:
-    try:
-        print()
-        q2a = int(input("Choose the wisely. > "))
-         # scoreect answer "3"
-        if q2a == 2:
-            score += 1
-            # if the user answers the question right the boolean turns true
-            # and gets out of the while loop
-            q2r = True
-        elif 0 < q2a < 5:
-            q2r = True
-            # If the user doesn't respond with a interger between 1 to 4
-        else:
-            print("please choose an interger from 1 to 4")
-            # If the user doesn't respond with a interger
-    except ValueError:
-         print("Invalid response.Please choose either option 1, 2, 3 or 4")
-         
-print()
-print("Your score",score + 0,"/10")
+#q1 
+q1c = False
+q1t = str("""Which gas is formed when a hydrogen bomb is detonated?
+1) Oxygen
+2) Methane
+3) Neon
+4) Helium""")
+qlr = 4
+
+# runs the question
+run_quest(q1t, q1c, 0, qlr)
+
+
+# score
+#print()
+#print("Your score",score + 0,"/10")
+
+
