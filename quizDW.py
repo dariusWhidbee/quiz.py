65a7da1bb43617ed069bc2ce94b029b36e39ad6f
csPython <cspython@AD25TF04NGF1J-78.local>
2019-01-16 12:15:07 -0500

    2nd question added and adjusted the score

--- quizDW.py
+++ quizDW.py
@@ -3,9 +3,11 @@
 # Variables
 score = 0
 q1r = False 
-q1a = 3
+q1a = 1
 
-
+score = 0
+q2r = False 
+q2a = 2
 
 # Introduction, asks the user 1 question
 print(" Hello, this program runs a quiz with 10 questions."
@@ -42,4 +44,31 @@ while q1r == False:
 
 
 print()
-print("Your score is", score + 0,"/ 10")
+print("""When I point up, it’s bright. When I point down, it’s dark.
+What am I?
+1) A flashlight
+2) A light switch
+3) A lamp
+4) The sun and moon""")
+
+while q2r == False:
+    try:
+        print()
+        q2a = int(input("Choose the wisely. > "))
+         # scoreect answer "3"
+        if q2a == 2:
+            score += 1
+            # if the user answers the question right the boolean turns true
+            # and gets out of the while loop
+            q2r = True
+        elif 0 < q2a < 5:
+            q2r = True
+            # If the user doesn't respond with a interger between 1 to 4
+        else:
+            print("please choose an interger from 1 to 4")
+            # If the user doesn't respond with a interger
+    except ValueError:
+         print("Invalid response.Please choose either option 1, 2, 3 or 4")
+         
+print()
+print("Your score",score + 0,"/10")
